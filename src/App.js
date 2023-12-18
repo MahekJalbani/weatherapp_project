467e7bccdcfeb5c7b135ecdfff51b3ff845b7260
Mahek Jalbani <mahek7660@gmail.com>
2023-12-18 16:36:03 +0500

    Searchbar created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import HeaderSec from './Assets/components/Header';   
+import 'bootstrap/dist/css/bootstrap.min.css';  
+import HeaderSec from './Assets/components/ Header';  
+import SearchLocation from './Assets/components/search';
 
 function App() {
   return (
     <div>
       <HeaderSec/>  
+      {/* <SearchLocation/> */}
     </div>
   );
 }
